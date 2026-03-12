a317ed7d67f78754d4d81ef0e8fe18c96ca20f8c
Hary Ny Aina RAJAONAH <hrajaonah@techmode.local>
2026-03-12 16:17:17 +0400

    execute file for import call et export auto

--- src/call/tasks/import-call-auto.task.ts
+++ src/call/tasks/import-call-auto.task.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../app.module';
-import { CallService } from '../../call/call.service';
+import { CallService } from '../call.service';
 
 async function bootstrap() {
 
